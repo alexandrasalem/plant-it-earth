d803361d74a48babee8c1351c261ade4a89c1617
pdxsam2 <samp2@pdx.edu>
2020-11-18 14:04:39 -0800

    Started working on a plant info page

--- routes/plant.js
+++ routes/plant.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+const { fileURLToPath } = require('url');
+const url = require('url'); 
+const token= 'u2q6v19zwHC_gIvReQn6mcJ7swYP0ysNVVHTVHQZsq8';
+const trefleUrl= 'https://trefle.io/api/v1/plants?token='+token;
+
 
 router.get('/', function (req,res) {
-    res.redirect()
+    res.status(302);
+    res.redirect('/');
 });
 
 router.get('/:id', function (req,res) {
-    res.status(200);
-    res.write("plant...");
-    res.send();
+    let id= req.url.splice('/');
+    console.log(queryObject, req.url);
+    console.log("HELLO!?");
+    // fetch()
+    res.render('plant', { title: 'A Plant!'});
 })
 
 module.exports = router;
