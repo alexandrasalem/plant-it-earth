cfc357ee34fd3a05e5ecf2c12683836b63aa8edf
pdxsam2 <samp2@pdx.edu>
2020-11-25 13:02:11 -0800

    cleaned up code, added some basic error handling

--- controllers/plant.js
+++ controllers/plant.js
@@ -15,7 +15,6 @@ async function getTrefle(trefleQuery) {
         cleanedData= {};
         for(property in data.data){
             if(!data.data[property]) continue;
-            // console.log(property);
             let desiredProperty= desiredSections.indexOf(property);
 
             if(desiredProperty === -1) continue;
@@ -24,12 +23,10 @@ async function getTrefle(trefleQuery) {
             cleanedData[`${propName}`]= data.data[`${property}`];
         }
         return cleanedData;
-        // console.log(cleanedData);
-        // res.render('plant', {result: cleanedData});
     };
 }
 
-//query the database for questions, responses, and users based on a plant id!
+//GET responses, questions, and users associated with a specific plant 
 async function getPlantQA(plantid){
     let result = await db.query(`
     SELECT u1.username, question_text, response_text, u2.username as r_username
@@ -45,8 +42,6 @@ async function getPlantQA(plantid){
     WHERE
         p.plant_id=${plantid}
     `);
-    // console.log(result.rows);
-    if(!result.rows) return null;
 
     let cleanedData= [];
     let questionStash= []; //used to check if a question already exists for this row
@@ -64,7 +59,6 @@ async function getPlantQA(plantid){
             let newResponse= {};
             newResponse.response= queryRow.response_text;
             newResponse.user= queryRow.r_username;    
-            // console.log(newResponse);
             for(let i= 0; i < cleanedData.length; ++i){
                 let QA= cleanedData[i];
                 if(QA.question == queryRow.question_text){
@@ -76,12 +70,11 @@ async function getPlantQA(plantid){
             }
         }
     }
-    // console.log(cleanedData);
     return cleanedData;
 }
 
+//POST questions implementation
 async function postPlantQ(username, question, plant_id) {
-    //this could use some cleaning, what if character cases are off?
     let result= await db.query(`
         SELECT user_id
         FROM users
@@ -94,17 +87,16 @@ async function postPlantQ(username, question, plant_id) {
         RETURNING user_id
         `);
     const user_id= result.rows[0].user_id;
-    // console.log(user_id,question, id);
     let postResult= await db.query(`
         INSERT INTO questions (user_id, question_text, general, plant_id, timestamp_question)
         VALUES (${user_id}, '${question}', 'no', ${plant_id}, NOW()) 
     `);
-    // console.log(postResult);
     return postResult;
 
 }
 
-async function postPlantR(username, question, response){
+//POST response implementation
+async function postPlantR(username, question, plant_id, response){
     let user_id= await db.query(`
         SELECT user_id
         From users
@@ -121,11 +113,11 @@ async function postPlantR(username, question, response){
 
     let question_id= await db.query(`
         SELECT question_id
-        FROM questions
+        FROM questions, plants
         WHERE question_text='${question}'
+        AND questions.plant_id=${plant_id}
     `);
     question_id= question_id.rows[0].question_id;
-    // console.log(user_id,question_id);
   
     let postResults= db.query(`
         INSERT INTO responses (question_id, user_id, response_text, timestamp_response)
@@ -133,53 +125,6 @@ async function postPlantR(username, question, response){
     `);
     return postResults;
 }
-
-//GET QUERY DRAFT
-/*
-    SELECT u1.username, question_text, response_text, u2.username as r_username
-    FROM users u1, users u2, questions q, responses r, plants p
-    WHERE
-        p.plant_id=182512
-        AND q.plant_id=p.plant_id
-        AND q.user_id=u1.user_id
-        AND r.question_id= q.question_id
-        AND r.user_id=u2.user_id 
-    ;
-
-    //Testing - this gets all the questions and their users for a specific plant
-    select username, question_text 
-    from users u, questions q, plants p,
-    where
-    p.plant_id=182512 
-    AND q.plant_id=p.plant_id
-    AND q.user_id=u.user_id;
-
-    //Testing - this gets all the responses and their users and questions for a specific plant
-    select username, question_text, response_text 
-    from users u, questions q, plants p,responses r
-    where
-    p.plant_id=182512 
-    AND q.plant_id=p.plant_id
-    AND q.user_id=u.user_id
-    AND r.question_id=q.question_id;
-*/
-
-//POST QUERY DRAFT (Q)
-/*
-    //check if this user exists!
-    select user_id from users where username=`${someName}
-            if not:
-                INSERT INTO users (username) VALUES ${someName} 
-                    RETURNING user_id
-    INSERT INTO questions (user_id, question_text, plant_id, timestamp_response)
-    VALUES (${userId}, ${question_text}, now());
-*/
-
-//POST QUERY DRAFT (R)
-/*
-    INSERT INTO responses (question_id, user_id, response_text, timestamp_response)
-postgres-> VALUES (1, 3, ‘I have done it without issues...’, now());
-*/
 
 exports.postPlantQ= postPlantQ;
 exports.postPlantR= postPlantR;
--- routes/plant.js
+++ routes/plant.js
@@ -17,20 +17,32 @@ router.get('/:id', function (req,res) {
 
     Promise.all([queries.getTrefle(trefleQuery),queries.getPlantQA(id)])
     .then(([trefleResult, dbResult]) => {
-        // console.log(dbResult);
         res.render('plant', {result:trefleResult, qa: dbResult});
+    })
+    .catch(err => {
+        res.render('error', { message: err});
     });
 });
 
 router.post('/:id', function(req,res) {
-    if(req.body.response){  //could maybe add a query here to verify that the question exists, i don't think that can happen... but ya know... code lines 
-        // console.log(req.body);
-        console.log(queries.postPlantR(req.body.user, req.body.question, req.body.response));
+    const id= (req.url.split('/'))[1];
+    if(req.body.response){
+        queries.postPlantR(req.body.user, req.body.question, id, req.body.response)
+        .then(res => {
+            res.redirect(req.originalUrl);
+        })
+        .catch(err => {
+            res.render('error', { message: err});
+        });
     }
     else if(req.body.question){
-        // console.log(req.body);
-        let id= (req.url.split('/'))[1];
-        console.log(queries.postPlantQ(req.body.user, req.body.question, id));
+        queries.postPlantQ(req.body.user, req.body.question, id)
+        .then(res => {
+            res.redirect(req.originalUrl);
+        })
+        .catch(err => {
+            res.render('error', { message: err});
+        });
     }
     else {}
     res.redirect(req.originalUrl);
@@ -38,27 +50,3 @@ router.post('/:id', function(req,res) {
 })
 
 module.exports = router;
-
-//VERBAL DESIGN:
-//**GET -- DONE
-//Check database for questions related to a plant_id
-// if yes:
-//      Find all questions and corresponding responses 
-//      AND USERS!
-//      toJSON and then pass to pug
-// if not: 
-//      don't pass anything 
-
-//**POST
-//Questions:
-//  Add a new field to questions (make sure to relate to user)
-// if user doesn't exist...
-//  add to user table
-
-//Responses:
-/*
-add entry to response table
-if user doesn't exist...
-    Add to user table
-//  Find id of question (query UGH) and add to response row
-*/
