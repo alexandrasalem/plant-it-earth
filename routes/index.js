58ef1425125de577b32b0caa2c88a8834bb28f96
pdxsam2 <samp2@pdx.edu>
2020-11-25 14:53:03 -0800

    merged with main

--- routes/index.js
+++ routes/index.js
@@ -21,22 +21,6 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/about", (req, res) => {
-    const aboutUs= `is an application dedicated to making gardening accessible 
-    to hobbyists and those looking to be self-sustaining alike. Especially with the prevalence
-    of the COVID-19 pandemic, more and more people are looking to try new things and we want to
-    provide those interested in gardening with space to learn and GROW! (I'm not sorry -Sam). 
-    Additionally, there are many environmental benefits to grow your own food and we want to 
-    facilitate that as much as possible. 
-    `;
-    const samAbout= `
-    4th-Year Computer Science undergrad student at Portland State University. Although I haven't
-    done much gardening while in college, it was a family past-time while growing up to plant in our
-    backyard. 
-    `;
-    res.render('about', {AboutUs: aboutUs, SamAbout: samAbout});
-})
-
 router.get("/test", (req, res) => {
   res.status(200);
   res.write("this is working this is WOOOOOOOORKIIIIIIIIIINGG");
