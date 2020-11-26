a02328d341a9ac8e0adb599765bbde13d24e3156
Alexandra Salem <alexandrasalem8@gmail.com>
2020-11-25 17:29:12 -0800

    printing out top users

--- controllers/mostpopular.js
+++ controllers/mostpopular.js
@@ -29,17 +29,19 @@ let plants_query = `
     ORDER BY count DESC
       `;
 let users_query = `
-    SELECT p.name, p.plant_id, count(*) as count
+    SELECT counts.username, SUM(counts.count) as count
+    FROM ((SELECT u1.username, u1.user_id, count(*) as count
     FROM users u1
     JOIN questions q
     ON q.user_id=u1.user_id
-    LEFT JOIN responses r
-    ON r.question_id= q.question_id
-    LEFT JOIN users u2
-    ON r.user_id = u2.user_id
-    JOIN plants p
-    ON p.plant_id = q.plant_id
-    GROUP BY p.plant_id
+    GROUP BY u1.user_id)
+    UNION
+    (SELECT u1.username, u1.user_id, count(*) as count
+    FROM users u1
+    JOIN responses r
+    ON r.user_id=u1.user_id
+    GROUP BY u1.user_id)) as counts
+    GROUP BY counts.username
     ORDER BY count DESC
   `;
 
@@ -80,4 +82,18 @@ async function getPopularPlants() {
   }
 }
 
+async function getPopularUsers() {
+  try {
+    let response = await db.query(users_query);
+    console.log(response.rows);
+    let names = [];
+    for (let i = 0; i < response.rows.length; ++i) {
+      names.push(`${response.rows[i].username} <br>`);
+    }
+    return names.join(" ");
+  } catch (error) {
+    console.log(error);
+  }
+}
 exports.getPopularPlants = getPopularPlants;
+exports.getPopularUsers = getPopularUsers;
--- routes/mostpopular.js
+++ routes/mostpopular.js
@@ -4,7 +4,11 @@ const queries = require("../controllers/mostpopular");
 
 router.get("/", async (req, res) => {
   var popularPlants = await queries.getPopularPlants();
-  res.render("mostpopular", { results: popularPlants });
+  var popularUsers = await queries.getPopularUsers();
+  res.render("mostpopular", {
+    results1: popularPlants,
+    results2: popularUsers,
+  });
 });
 
 module.exports = router;
