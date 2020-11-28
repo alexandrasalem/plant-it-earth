916d03b3a5e39a70ca98d7e4a805855302c59128
Alexandra Salem <alexandrasalem8@gmail.com>
2020-11-27 17:40:03 -0800

    updating search structure for optional full search

--- controllers/search.js
+++ controllers/search.js
@@ -3,39 +3,30 @@ const { token } = require("../credentials.js");
 
 var url = `https://trefle.io/api/v1/plants/search?token=${token}`;
 
-async function searchOne(request) {
-  url = `${url}&q=${request.body.veg}`;
+async function searchOne(search_veg) {
+  url = `${url}&q=${search_veg}`;
   var data = await fetch(url);
   var data = await data.json();
-  let common_names = [];
+  let search_results = [];
   for (let index = 0; index < data.data.length; index++) {
     if (
       data.data[index].common_name != null &&
       data.data[index].common_name
         .toLowerCase()
-        .includes(request.body.veg.toLowerCase())
+        .includes(search_veg.toLowerCase())
     ) {
-      common_names.push(
-        `<article class="media">
-      <figure class="media-left">
-        <p class="image is-64x64">
-          <img src="${data.data[index].image_url}" alt = "${data.data[index].common_name}">
-        </p>
-      </figure>
-      <div class="media-content">
-        <div class="content">
-          <a href = "/plant/${data.data[index].id}">${data.data[index].common_name}</a>
-        </div>
-      </div>
-    </article>`
-      );
+      search_results.push([
+        data.data[index].common_name,
+        data.data[index].image_url,
+        data.data[index].id,
+      ]);
     }
   }
-  return common_names.join("");
+  return search_results;
 }
 
-async function searchAll(request) {
-  url = `${url}&q=${request.body.continue}`;
+async function searchAll(search_veg) {
+  url = `${url}&q=${search_veg}`;
   var data = await fetch(url);
   var data = await data.json();
   var allData = data;
@@ -49,31 +40,24 @@ async function searchAll(request) {
     allData.meta = newData.meta;
     console.log(next);
   }
-  common_names = [];
+
+  search_results = [];
   for (let index = 0; index < allData.data.length; index++) {
     if (
       allData.data[index].common_name != null &&
       allData.data[index].common_name
         .toLowerCase()
-        .includes(request.body.continue.toLowerCase())
+        .includes(search_veg.toLowerCase())
     ) {
-      common_names.push(
-        `<article class="media">
-      <figure class="media-left">
-        <p class="image is-64x64">
-          <img src="${allData.data[index].image_url}" alt = "${allData.data[index].common_name}">
-        </p>
-      </figure>
-      <div class="media-content">
-        <div class="content">
-          <a href = "/plant/${allData.data[index].id}">${allData.data[index].common_name}</a>
-        </div>
-      </div>
-    </article>`
-      );
+      search_results.push([
+        data.data[index].common_name,
+        data.data[index].image_url,
+        data.data[index].id,
+      ]);
     }
   }
-  return common_names.join("");
+
+  return search_results;
 }
 
 exports.searchAll = searchAll;
--- routes/search.js
+++ routes/search.js
@@ -7,18 +7,21 @@ router.get("/", (req, res) => {
   res.render("search");
 });
 
-router.post("/", async (req, res) => {
-  console.log(req.body);
-  if (req.body.continue != undefined) {
-    console.log(req.body.continue);
-    let fullResults = await queries.searchAll(req);
-    res.render("search", { fullResults: `${fullResults}` });
-  } else {
-    let results = await queries.searchOne(req);
-    res.render("search", {
-      results: `${results}`,
-    });
-  }
+router.get("/:veg", async (req, res) => {
+  let search_veg = req.url.split("/")[1];
+  let results = await queries.searchOne(search_veg);
+  console.log(results);
+  res.render("search", {
+    results: results,
+  });
+});
+
+router.get("/:veg/full", async (req, res) => {
+  let search_veg = req.url.split("/")[1];
+  let fullResults = await queries.searchAll(search_veg);
+  res.render("search", {
+    fullResults: fullResults,
+  });
 });
 
 module.exports = router;
