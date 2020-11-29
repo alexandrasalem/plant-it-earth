9079be0fb5e015fa0aafc296f10b179904c0b3ae
Tesedy <tatiana@mercari.com>
2020-11-29 12:20:27 -0800

    Adjusted dataExract script to reflect API page layout for data extraction

--- routes/usda.js
+++ routes/usda.js
@@ -19,20 +19,34 @@ router.get("/", (req, res) => {
 
 router.post("/", async (req, res) => {
   
-  console.log(req);
+  if(req.body.zip == "") {
+    res.render("usda", {
+      error: `Zip can not be blank`
+    });
+  }
+  else {
 
-  db.query('select * from usda_zones where zipcode = ' + req.body.zip + ';', (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send(err);
-    } else {
-      res.render("usda", {
-        results: `${results.rows[0].zone}`,
-      });
-    }
-  });
+    db.query('select * from usda_zones where zipcode = ' + req.body.zip + ';', (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send(err);
+      } else {
 
-  
+        //do the second query for the plants we can grow
+        db.query("select * from plants_data where min_zone = '" + results.rows[0].zone + "';", (err2, results2) => {
+          if (err2) {
+            console.error(err2);
+            res.status(500).send(err2);
+          } else {
+            res.render("usda", {
+              results: `${results.rows[0].zone}`,
+              zoneData: results2.rows
+            });
+          }
+        });
+      }
+    });
+  }
 });
 
 module.exports = router;
